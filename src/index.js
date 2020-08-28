d11cd2a9dc8013a64034407b968a82f0cc57eb44
andreivirko <andreithebro231@gmail.com>
2020-08-28 11:13:24 +0300

    api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import "./styles/styles.css";
 // import "./timer/timer.js";
 // import "./menu/menu.js";
-import './promisification/promise.js';
-
+import "./promisification/promise.js";
 
 // const url = "https://restcountries.eu/rest/v2/all";
 // GET- запрос на получение данных
@@ -23,33 +22,76 @@ import './promisification/promise.js';
 //   .then((data) => {
 //     console.log(data);
 
-    // MAP //
-    // data.map((item) => console.log(item.name));
+// MAP //
+// data.map((item) => console.log(item.name));
 
-    // FILTER //
-    // data.filter((item) => {
-    //   if (item.name.includes("w")) return console.log(item);
-    // })
+// FILTER //
+// data.filter((item) => {
+//   if (item.name.includes("w")) return console.log(item);
+// })
 
-    // FIND //
-    // data.find(item => {if(item.name.includes("-")) return console.log(item)});
+// FIND //
+// data.find(item => {if(item.name.includes("-")) return console.log(item)});
 
-    // SORT //
-  //   return console.log(
-  //     data
+// SORT //
+//   return console.log(
+//     data
 
-  //       .sort((min, max) => {
-  //         max.population - min.population;
-  //       })
-  //       .filter((item) => item.name.includes("j"))
-  //       .map((item) => item.name),
-  //   );
-  // })
-  // .catch((error) => {
-  //   console.log(error);
-  // });
+//       .sort((min, max) => {
+//         max.population - min.population;
+//       })
+//       .filter((item) => item.name.includes("j"))
+//       .map((item) => item.name),
+//   );
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
 
 //   const flag = document.createElement("img");
 //   flag.src = "https://restcountries.eu/data/tls.svg";
 //   const body = document.querySelector("body");
 //   body.appendChild(flag);
+
+// const url = "https://cat-fact.herokuapp.com/facts";
+
+// // fetch("https://cat-fact.herokuapp.com");
+// fetch(url)
+// .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// запрос аунтефикации //
+
+// let queryCat = "javascript";
+// let queryLang = "en";
+// let pageSize = 20;
+// let page = 1;
+
+// const apiKey = "2b939a2eba6947788e0aaf3b2c375927";
+// const maniUrl = "https://newsapi.org/";
+// const versionDb = "v2/";
+// const endpoint = "everything";
+// const queries = `?q=${queryCat}&language=${queryLang}&pageSize=${pageSize}&page=${page}`;
+
+// const url = maniUrl + versionDb + endpoint + queries;
+import template from "./templates/menuItem.hbs";
+const url = `https://restcountries.eu/rest/v2/all`;
+const endPoint = "/name";
+console.log(url);
+
+// const options = {
+//   headers: {
+//     Authirization: apiKey,
+//   },
+// };
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    const item = template(data);
+    console.log(item);
+    ul.insertAdjacentHTML("afterbegin", item);
+  });
+
+  const ul = document.querySelector(".flags");
--- src/promisification/promise.js
+++ src/promisification/promise.js
@@ -1,148 +1,148 @@
+// // const promise = new Promise((resolve, reject) => {
+// // resolve('succseful');
+// // reject('error');
+// // })
+// // console.log(promise);
+
+// // promise.then(data => console.log(data)).catch(error => console.log(error));
+// // const promise1 = new Promise((resolve, reject) => {
+// //     resolve(10);
+// // })
+
+// // promise1.then((value) => {
+// //     value += 5;
+// //     console.log(value);
+// //     return value
+// // }).then((value) => {
+// //     value -= 5;
+// //     console.log(value);
+// //     return value
+// // }).catch((error) => {
+// //     console.log(error);
+// // })
+
+// // function promisification(value) {
+// //     const promise = new Promise((resolve, reject) => {
+// //         if (value > 10) {
+// //             resolve(console.log('succsesful'));
+// //         } else {
+// //             reject(console.error('error'))
+// //         }
+// //     })
+// //     return promise
+// // }
+// //     promisification(11).then(data => console.log(data));
+
+//     // task 1 // 
+
+// //     const delay = ms => {
+// //   const promise = new Promise((resolve, reject) => {
+// //     setTimeout(() => {
+// //       resolve(ms);
+// //       reject()
+// //     }, ms);
+// //   })
+// //   return promise;
+// // };
+
+// // const logger = time => console.log(`Resolved after ${time}ms`);
+
+// // // Вызовы функции для проверки
+// // delay(2000).then(logger); // Resolved after 2000ms
+// // delay(1000).then(logger); // Resolved after 1000ms
+// // delay(1500).then(logger); // Resolved after 1500ms
+
+
+// // task 2 //
+
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName, callback) => {
 // const promise = new Promise((resolve, reject) => {
-// resolve('succseful');
-// reject('error');
-// })
-// console.log(promise);
+//   setTimeout(() => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+// )
+//   resolve(updatedUsers);
+//   reject((error) => console.log('error'));
+//   },0);
 
-// promise.then(data => console.log(data)).catch(error => console.log(error));
-// const promise1 = new Promise((resolve, reject) => {
-//     resolve(10);
-// })
+// });
+// return promise;
 
-// promise1.then((value) => {
-//     value += 5;
-//     console.log(value);
-//     return value
-// }).then((value) => {
-//     value -= 5;
-//     console.log(value);
-//     return value
-// }).catch((error) => {
-//     console.log(error);
-// })
-
-// function promisification(value) {
-//     const promise = new Promise((resolve, reject) => {
-//         if (value > 10) {
-//             resolve(console.log('succsesful'));
-//         } else {
-//             reject(console.error('error'))
-//         }
-//     })
-//     return promise
-// }
-//     promisification(11).then(data => console.log(data));
-
-    // task 1 // 
-
-//     const delay = ms => {
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(ms);
-//       reject()
-//     }, ms);
-//   })
-//   return promise;
 // };
+// // toggleUserState();
+// const logger = updatedUsers => console.table(updatedUsers);
 
-// const logger = time => console.log(`Resolved after ${time}ms`);
+// /*
+//  * Сейчас работает так
+//  */
+// // toggleUserState(users, 'Mango', logger);
+// // toggleUserState(users, 'Lux', logger);
 
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+// /*
+//  * Должно работать так
+//  */
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
 
+// // task 3 //
 
-// task 2 //
+// // Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
 
-const users = [
-  { name: 'Mango', active: true },
-  { name: 'Poly', active: false },
-  { name: 'Ajax', active: true },
-  { name: 'Lux', active: false },
-];
+// // const randomIntegerFromInterval = (min, max) => {
+// //   return Math.floor(Math.random() * (max - min + 1) + min);
+// // };
 
-const toggleUserState = (allUsers, userName, callback) => {
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-)
-  resolve(updatedUsers);
-  reject((error) => console.log('error'));
-  },0);
+// // const makeTransaction = (transaction, onSuccess, onError) => {
+// //   const delay = randomIntegerFromInterval(200, 500);
 
-});
-return promise;
+// //   setTimeout(() => {
+// //     const canProcess = Math.random() > 0.3;
 
-};
-// toggleUserState();
-const logger = updatedUsers => console.table(updatedUsers);
+// //     if (canProcess) {
+// //       onSuccess(transaction.id, delay);
+// //     } else {
+// //       onError(transaction.id);
+// //     }
+// //   }, delay);
+// // };
 
-/*
- * Сейчас работает так
- */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
+// // const logSuccess = (id, time) => {
+// //   console.log(Transaction ${id} processed in ${time}ms);
+// // };
 
-/*
- * Должно работать так
- */
-toggleUserState(users, 'Mango').then(logger);
-toggleUserState(users, 'Lux').then(logger);
+// // const logError = id => {
+// //   console.warn(Error processing transaction ${id}. Please try again later.);
+// // };
 
-// task 3 //
+// // /*
+// //  * Работает так
+// //  */
+// // makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// // makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// // makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// // makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+// // /*
+// //  * Должно работать так
+// //  */
+// // makeTransaction({ id: 70, amount: 150 })
+// //   .then(logSuccess)
+// //   .catch(logError);
 
-// Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
+// // makeTransaction({ id: 71, amount: 230 })
+// //   .then(logSuccess)
+// //   .catch(logError);
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+// // makeTransaction({ id: 72, amount: 75 })
+// //   .then(logSuccess)
+// //   .catch(logError);
 
-const makeTransaction = (transaction, onSuccess, onError) => {
-  const delay = randomIntegerFromInterval(200, 500);
-
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
-
-    if (canProcess) {
-      onSuccess(transaction.id, delay);
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
-};
-
-const logSuccess = (id, time) => {
-  console.log(Transaction ${id} processed in ${time}ms);
-};
-
-const logError = id => {
-  console.warn(Error processing transaction ${id}. Please try again later.);
-};
-
-/*
- * Работает так
- */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
-makeTransaction({ id: 70, amount: 150 })
-  .then(logSuccess)
-  .catch(logError);
-
-makeTransaction({ id: 71, amount: 230 })
-  .then(logSuccess)
-  .catch(logError);
-
-makeTransaction({ id: 72, amount: 75 })
-  .then(logSuccess)
-  .catch(logError);
-
-makeTransaction({ id: 73, amount: 100 })
-  .then(logSuccess)
-  .catch(logError);
+// // makeTransaction({ id: 73, amount: 100 })
+// //   .then(logSuccess)
+// //   .catch(logError);
